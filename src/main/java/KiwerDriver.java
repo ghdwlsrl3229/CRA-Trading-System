0c26ef752c2628d7c74ebd6dd8077919550fc80c
ghdwlsrl3229 <ghdwlsrl3229@naver.com>
2024-05-02 15:47:19 +0900

    kiwer driver first commit

---
+++ src/main/java/KiwerDriver.java
@@ -0,0 +1,25 @@
+public class KiwerDriver implements StockerBrockerDriver {
+    private boolean isLogin;
+
+    public boolean isLogIn() {
+        return isLogin;
+    }
+
+    @Override
+    public void login(String id, String pass){
+    }
+
+    @Override
+    public void buy(String stockCode, int count, int price) {
+    }
+
+    @Override
+    public void sell(String stockCode, int count, int price) {
+    }
+
+    @Override
+    public int getPrice(String stockCode) {
+        return 0;
+    }
+
+}
