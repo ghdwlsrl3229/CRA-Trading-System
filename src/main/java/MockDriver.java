a4ddf1a8c2fa39ec54d9a56a9a184e6f2a4f7ae5
taylous <taylous99@gmail.com>
2024-05-02 10:41:04 +0900

    [Add] implement methods

--- src/main/java/MockDriver.java
+++ src/main/java/MockDriver.java
@@ -1,3 +1,22 @@
 public class MockDriver implements StockerBrockerDriver {
 
+    @Override
+    public void login(String id, String pass) {
+
+    }
+
+    @Override
+    public void buy(String stockCode, int count, int price) {
+        System.out.println("stockCode: " + stockCode + " count: " + count + " price: " + price);
+    }
+
+    @Override
+    public void sell(String stockCode, int count, int price) {
+
+    }
+
+    @Override
+    public int getPrice(String stockCode) {
+        return 0;
+    }
 }
