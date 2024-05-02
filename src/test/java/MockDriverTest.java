c898d79ac349af9b9ffb79858a60b57edcf666f6
taylous <taylous99@gmail.com>
2024-05-02 13:18:16 +0900

    [Add] buy mocking test 코드

--- src/test/java/MockDriverTest.java
+++ src/test/java/MockDriverTest.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MockDriverTest {
+
     MockDriver mockDriver;
 
     @BeforeEach
@@ -18,12 +20,35 @@ class MockDriverTest {
 
     @Test
     void loginTest() {
-        mockDriver.login("id", "pass");
+        mockDriver.login("id1", "pass1");
+
+        assertEquals(1, mockDriver.getLoginCount());
     }
 
     @Test
-    void buyTest() {
-        mockDriver.buy("code", 2, 3);
+    void buy_normal_test() {
+        mockDriver.buy("code", 1, 1);
+    }
+
+    @Test
+    void buy_not_login() {
+        assertThrows(IllegalStateException.class, () -> {
+            mockDriver.buy("code", 2, 3);
+        });
+    }
+
+    @Test
+    void buy_invalid_price() {
+        assertThrows(IllegalStateException.class, () -> {
+            mockDriver.buy("code", 2, 0);
+        });
+    }
+
+    @Test
+    void buy_invalid_count() {
+        assertThrows(IllegalStateException.class, () -> {
+            mockDriver.buy("code", 0, 3);
+        });
     }
 
     @Test
@@ -31,11 +56,15 @@ class MockDriverTest {
         String stockCode = "code";
         int count = 2;
         int price = 4;
-        mockDriver.sell(stockCode, count, price);
+        mockDriver.sell("code", count, price);
+
+        assertEquals(count * price, mockDriver.getMoney());
+        assertEquals(-count, mockDriver.getStockCount(stockCode));
     }
 
     @Test
     void getPriceTest() {
         assertEquals(0, mockDriver.getPrice("code"));
+        assertEquals(1, mockDriver.getLoginCount());
     }
 }
