91d2756cc9c521bf2f2e853df9e966d8877b8502
taylous <taylous99@gmail.com>
2024-05-02 15:16:29 +0900

    [Add & Modify] auto trading system buy 테스트 코드

--- src/main/java/AutoTradingSystem.java
+++ src/main/java/AutoTradingSystem.java
@@ -4,4 +4,19 @@ public class AutoTradingSystem {
     public void selectStockerBrocker(StockerBrockerDriver driver) {
         this.stockerBrockerDriver = driver;
     }
+
+    public void login(String id, String pass) {
+
+    }
+
+    public void buy(String code, int count, int price) {
+        this.stockerBrockerDriver.buy(code, count, price);
+    }
+
+    public void sell(String code, int i, int i1) {
+    }
+
+    public int getPrice(String code) {
+        return 0;
+    }
 }
--- src/test/java/AutoTradingSystemTest.java
+++ src/test/java/AutoTradingSystemTest.java
@@ -4,12 +4,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class AutoTradingSystemTest {
+
     AutoTradingSystem ats;
+
     @Mock
     MockDriver driver;
 
@@ -28,14 +32,47 @@ class AutoTradingSystemTest {
     void loginTest() {
         ats.login("id", "pass");
 
-        verify(driver, times(1)).login("id", "pass");
+        verify(driver, times(1)).login("id", "pass").;
     }
 
     @Test
-    void buyTest() {
+    void buy_normal_test() {
         ats.buy("code", 2, 3);
 
         verify(driver, times(1)).buy("code", 2, 3);
+    }
+
+    @Test
+    void buy_null_stock_code() {
+        try {
+            ats.buy(null, 2, 3);
+        }
+        catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("stock code 를 입력해주세요.");
+        }
+        verify(driver, times(1)).buy(null, 2, 3);
+    }
+
+    @Test
+    void buy_invalid_count() {
+        try {
+            ats.buy("code", 0, 3);
+        }
+        catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("count(개수)는 최소 1 이상의 값이어야 합니다.");
+        }
+        verify(driver, times(1)).buy("code", 0, 3);
+    }
+
+    @Test
+    void buy_invalid_price() {
+        try {
+            ats.buy("code", 2, 0);
+        }
+        catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("price(금액)는 최소 1 이상의 값이어야 합니다.");
+        }
+        verify(driver, times(1)).buy("code", 2, 0);
     }
 
     @Test
@@ -51,6 +88,4 @@ class AutoTradingSystemTest {
 
         verify(driver, times(1)).getPrice("code");
     }
-
-
 }
