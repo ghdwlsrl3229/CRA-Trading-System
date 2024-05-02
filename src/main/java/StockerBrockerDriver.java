ee7191eef05921debe4be9ca51552f6026c77e7e
hyunjung <gusejddl519@naver.com>
2024-05-02 10:21:33 +0900

    StockerBrockerDriver 파일 생성

---
+++ src/main/java/StockerBrockerDriver.java
@@ -0,0 +1,7 @@
+public interface StockerBrockerDriver {
+
+    void login(String id, String pass);
+    void buy(String stockCode, int count , int price);
+    void sell( String stockCode , int count , int price);
+    int getPrice ( String stockCode);
+}
